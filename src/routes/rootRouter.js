be3726d5f7381d5f6d003b107d810c5b7a66078d
dovakeen118 <kegillis118@gmail.com>
2023-09-25 12:16:04 -0400

    with other examples

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -3,11 +3,13 @@ import express from "express"
 const rootRouter = new express.Router()
 import tasksRouter from "./tasksRouter.js"
 
+// localhost:3000
+
+// rootRouter.use("/cats", catsRouter)
 rootRouter.use("/tasks", tasksRouter)
 
-rootRouter.get('/', (req, res) => {
+rootRouter.get("/", (req, res) => {
   res.send("the true root route")
 })
-
 
 export default rootRouter
--- src/routes/tasksRouter.js
+++ src/routes/tasksRouter.js
@@ -1,25 +1,42 @@
 import express from "express"
 
+// all routes starting with "/tasks" come to this router (rootRouter!)
 const tasksRouter = new express.Router()
 
 // tasksRouter.get("/example", (req, res) => {
-//   // grab stuff from params if needed 
+//   // grab stuff from params if needed
 
 //   // perform business logic if needed
 
-//   // render template 
+//   // render template
 // })
 
+// "/tasks"
 tasksRouter.get("/", (req, res) => {
-  const someTasks = ["smell the flowers", "walk around", "dont think about....all of it...the world... everything"]
+  const someTasks = [
+    "smell the flowers",
+    "walk",
+    "dont think about....all of it...the world... everything",
+    "pet the cat"
+  ]
 
-  res.render('index', { tasks: someTasks })
+  // view file, any data
+  res.render("index", { tasks: someTasks })
+  // res.render("index", { tasks: someTasks, message: "hello!" })
+  // res.render("tasks/index", { tasks: someTasks })
 })
 
+// "/tasks/walk"
 tasksRouter.get("/:taskName", (req, res) => {
+  // console.log(req)
+  console.log("params", req.params)
   const task = req.params.taskName
-  res.render("show", { task: task })
+  // res.render("show", { forTheTemplate: task })
+  const myTaskObject = {
+    name: task,
+    due: "tomorrow"
+  }
+  res.render("show", { forTheTemplate: myTaskObject })
 })
-
 
 export default tasksRouter
